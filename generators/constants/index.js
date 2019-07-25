9c642ea7650e4d6e4f6b4f90e25a3369a90d59e3
Blair Calderwood <blaircalderwood@live.co.uk>
2019-07-25 15:52:50 +0100

    Added constants subgenerator

---
+++ generators/constants/index.js
@@ -0,0 +1,32 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+const { toKebabCase } = require('../../utils/strings.util');
+
+module.exports = class extends Generator {
+  prompting() {
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'constantsName',
+        message: 'Constants name:',
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    const { constantsName } = this.props;
+    const fileName = toKebabCase(constantsName);
+
+    this.fs.copy(
+      this.templatePath('./constants-name.js'),
+      this.destinationPath(`./src/constants/${fileName}.js`),
+    );
+  }
+};
