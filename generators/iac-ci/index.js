623a72ec0fba7dadb0b8b010831d8706050d51e1
Blair Calderwood <blaircalderwood@live.co.uk>
2019-08-21 16:18:01 +0100

    Added validation for IAC prompts

--- generators/iac-ci/index.js
+++ generators/iac-ci/index.js
@@ -9,21 +9,25 @@ module.exports = class extends Generator {
         type: 'input',
         name: 'pipelineName',
         message: 'Pipeline name:',
+        validate: this._checkLength,
       },
       {
         type: 'input',
         name: 'awsRegion',
         message: 'AWS Region:',
+        validate: this._isValidRegion,
       },
       {
         type: 'input',
         name: 'awsAccountNumber',
         message: 'AWS Account Number:',
+        validate: this._isNumber,
       },
       {
         type: 'input',
         name: 'gitRepo',
         message: 'Git respository for build source (HTTPS):',
+        validate: this._isValidUrl,
       },
     ];
 
@@ -69,5 +73,27 @@ module.exports = class extends Generator {
       this.destinationPath(`../scripts/`),
       mappings
     );
+  }
+
+  _checkLength(str) {
+    return str.length < 50
+      ? true
+      : 'Length limit of 50 characters exceeded. Please choose a shorter name.';
+  }
+
+  _isNumber(str) {
+    return isNaN(str) ? 'Not a valid number.' : true;
+  }
+
+  _isValidRegion(str) {
+    const regionRegex = /^[a-z][a-z]-[a-z]*-[0-9]{1}/;
+
+    return str.match(regionRegex) ? true : 'Not a valid AWS region.';
+  }
+
+  _isValidUrl(str) {
+    const regionRegex = /https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+    return str.match(regionRegex) ? true : 'Not a valid HTTPS URL.';
   }
 };
