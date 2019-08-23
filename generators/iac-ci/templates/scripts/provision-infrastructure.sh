a3e500bf34f9f052d0a76aa103a52cc4ebbb85a5
Blair Calderwood <blaircalderwood@live.co.uk>
2019-08-23 13:15:00 +0100

    Added auto-approve to MakeFile

--- generators/iac-ci/templates/scripts/provision-infrastructure.sh
+++ generators/iac-ci/templates/scripts/provision-infrastructure.sh
@@ -3,10 +3,15 @@
 ## This script builds an array of folders and subfolders within ./iac/ which contain the file 'remote_state.tf'.
 ## It then loops through these and runs the terraform Makefile to apply that particular terraform deployment.
 
-terraform_component_array=(`find ../iac/ -type f -name '*remote_state.tf'`)
+# terraform_component_array=(`find ../iac/ -type f -name '*remote_state.tf'`)
 
-for component in "${terraform_component_array[@]}"
-do :
-    COMPONENT=(`echo $component | sed -e "s/^..\/iac\/\///" -e 's/\(\/remote_state.tf\)*$//g'`);
-    cd ../iac/ && COMPONENT=$COMPONENT Make apply
-done
+# for component in "${terraform_component_array[@]}"
+# do :
+#     COMPONENT=(`echo $component | sed -e "s/^..\/iac\/\///" -e 's/\(\/remote_state.tf\)*$//g'`);
+#     cd ../iac/ && COMPONENT=$COMPONENT Make apply
+# done
+
+cd ../iac/ && COMPONENT=codebuild/build Make apply
+cd ../iac/ && COMPONENT=codebuild/deploy-dev Make apply
+cd ../iac/ && COMPONENT=codebuild/deploy-test Make apply
+cd ../iac/ && COMPONENT=codepipeline Make apply
